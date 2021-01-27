47d08b734f94fa60e0a684e592962a1435339235
Emre <emreguclu@hotmail.com>
2021-01-27 15:22:54 +0300

    Updated

---
+++ Demo-1.ps1
@@ -0,0 +1,38 @@
+#Requires -Version 7.0
+#Requires -Modules @{ModuleName='Az';ModuleVersion='5.4.0'},@{ModuleName='Az.ConnectedMachine';ModuleVersion='0.2.0'}
+
+$ParametersFile = './Parameters.psd1'
+$Parameters=Import-PowerShellDataFile -Path $ParametersFile
+$VMs = $Parameters.VMs
+$ArcEnabledServers = $Parameters.ArcEnabledServers
+$ResourceGroupName = $Parameters.ResourceGroupName
+$SubscriptionID = $Parameters.SubscriptionID
+$DCRName = $Parameters.DCRName
+
+
+# create a data collection rule Named in Variable $DCRName in the portal and associate with VM1
+
+# Get Extension List for the VMs and verify the extension is installed
+$VMs | ForEach-Object {Get-AzVMExtension -VMName $_ -ResourceGroupName $ResourceGroupName | Select-Object -Property VMName,Name,ProvisioningState,Statuses}
+
+# Check extensions in portal and add ArcEnabledServer1 to to assoication in the portal
+
+# Get Extension List for Arc Servers
+$ArcEnabledServers | ForEach-Object {$MachineName = $_ ;Get-AzConnectedMachineExtension -MachineName $_ -ResourceGroupName $ResourceGroupName -PipelineVariable $ExtensionInfo | Select-Object -Property @{Name='MachineName';Expression={$MachineName}},Name,ProvisioningState}
+
+# Time for a take 100 in Workspace in the portal
+
+# Install The Extension Using Powershell for VMs
+Set-AzVMExtension -Name AMAWindows -ExtensionType AzureMonitorWindowsAgent -Publisher Microsoft.Azure.Monitor -ResourceGroupName $ResourceGroupName -VMName $VMs[1] -Location 'Eastus'
+
+# Install The Extension Using Powershell for Azure Arc Enabled Servers
+New-AzConnectedMachineExtension -Name AMAWindows -ExtensionType AzureMonitorWindowsAgent -Publisher Microsoft.Azure.Monitor -ResourceGroupName $ResourceGroupName -MachineName $ArcEnabledServers[1] -Location 'Eastus'
+
+# Reuse the above Powershell cmdlets to list extensions for the servers added.
+
+# Create DCR Association using Powershell
+$dcr = Get-AzDataCollectionRule -ResourceGroupName $ResourceGroupName -RuleName $dcrName
+$vmId = "/subscriptions/$SubscriptionID/resourceGroups/$ResourceGroupName/providers/Microsoft.Compute/virtualMachines/$($VMs[1])"
+$ArcEnabledServerID = "/subscriptions/$SubscriptionID/resourceGroups/$ResourceGroupName/providers/Microsoft.hybridcompute/virtualMachines/$($ArcEnabledServers[1])"
+$dcr | New-AzDataCollectionRuleAssociation -TargetResourceId $ArcEnabledServerID  -AssociationName "PowerShellBasedAssocArc"
+$dcr | New-AzDataCollectionRuleAssociation -TargetResourceId $VMID -AssociationName "PowerShellBasedAssocVM"
---
+++ Parameters.psd1
@@ -0,0 +1,9 @@
+@{
+    VMs = @('emreg-web01','emreg-web02')
+    ArcEnabledServers = @('web03','web04')
+    ResourceGroupName = 'ContosoAll'
+    Location = 'EastUs'
+    SubscriptionID = 'c02646f3-6401-40c7-8543-69333821da9a'
+    DCRName = 'Default Collection'
+    WorkspaceName = 'Spark21ws1'
+}
